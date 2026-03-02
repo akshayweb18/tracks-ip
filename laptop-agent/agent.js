cc21e22b83e35af843b9c7b14450357a9fe08eb8
akshayc-itm <akshayc@itm.edu>
2026-03-02 13:22:22 +0530

    agent updated

--- laptop-agent/agent.js
+++ laptop-agent/agent.js
@@ -1,3 +1,40 @@
+// // laptop-agent/agent.js
+// const axios = require("axios");
+// const os = require("os");
+// const publicIp = require("public-ip");
+
+// async function sendHeartbeat() {
+//   try {
+//     // Get local IP
+//     const localIP = Object.values(os.networkInterfaces())
+//       .flat()
+//       .find((i) => i.family === "IPv4" && !i.internal)?.address;
+
+//     // Get public IP
+//     const pubIP = await publicIp.v4();
+
+//     // Device name = laptop name
+//     const deviceId = os.hostname();
+
+//     await axios.post("http://localhost:3000/api/heartbeat", { 
+//       deviceId,
+//       assignedEmployee: deviceId, // you can change this if needed
+//       publicIP: pubIP,
+//       localIP,
+//     });
+
+//     console.log("Heartbeat sent for", deviceId);
+//   } catch (err) {
+//     console.error("Heartbeat error:", err.message);
+//   }
+// }
+
+// // Send immediately
+// sendHeartbeat();
+
+// // Repeat every 30 seconds
+// setInterval(sendHeartbeat, 30000);
+
 // laptop-agent/agent.js
 const axios = require("axios");
 const os = require("os");
@@ -16,7 +53,7 @@ async function sendHeartbeat() {
     // Device name = laptop name
     const deviceId = os.hostname();
 
-    await axios.post("http://localhost:3000/api/heartbeat", {
+    await axios.post("https://tracks-ip-svq8.vercel.app/api/heartbeat", { 
       deviceId,
       assignedEmployee: deviceId, // you can change this if needed
       publicIP: pubIP,
